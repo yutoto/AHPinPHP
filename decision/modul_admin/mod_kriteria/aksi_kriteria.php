f51f1ad7527f1e03ea872b70d4c866c8d6942e96
Adi <undip.nugroho@gmail.com>
2015-11-13 01:41:52 +1030

    Update aksi_kriteria.php

--- decision/modul_admin/mod_kriteria/aksi_kriteria.php
+++ decision/modul_admin/mod_kriteria/aksi_kriteria.php
@@ -5,7 +5,7 @@ include "../../config/koneksi.php";
 $modul=$_GET[modul];
 $act=$_GET[act];
 
-// Hapus kriteria
+// Delete Criteria
 if ($modul=='kriteria' AND $act=='hapus'){
   mysql_query("DELETE FROM kriteria WHERE id_kriteria='$_GET[id]'");
   mysql_query("DELETE FROM konsistensi");
@@ -13,7 +13,7 @@ if ($modul=='kriteria' AND $act=='hapus'){
   header('location:../../indexs.php?modul='.$modul);
 }
 
-// Input kriteria
+// Input Criteria
 elseif ($modul=='kriteria' AND $act=='input'){
   mysql_query("INSERT INTO kriteria(id_kriteria, nama_kriteria) VALUES('$_POST[id_kriteria]', '$_POST[nama_kriteria]')");
   mysql_query("INSERT INTO bobot_kriteria(id_kriteria, nama_kriteria) VALUES('$_POST[id_kriteria]', '$_POST[nama_kriteria]')");
@@ -24,13 +24,13 @@ elseif ($modul=='kriteria' AND $act=='input'){
     $nip = $row["nip"];
     mysql_query("INSERT INTO evaluasi(nip, id_kriteria, nilai) VALUES('$nip', '$_POST[id_kriteria]', '$_POST[nama_kriteria]')");
   }
-  // Hapus konsistensi dan hasil evaluasi
+  // Delete Consistency value dan Evaluation Result
   mysql_query("DELETE FROM konsistensi");
   mysql_query("DELETE FROM hasil_evaluasi");
   header('location:../../indexs.php?modul='.$modul);
 }
 
-// Update kriteria
+// Update Criteria
 elseif ($modul=='kriteria' AND $act=='update'){
   mysql_query("UPDATE kriteria SET nama_kriteria = '$_POST[nama_kriteria]' WHERE id_kriteria = '$_POST[id]'");
   mysql_query("UPDATE bobot_kriteria SET nama_kriteria = '$_POST[nama_kriteria]' WHERE id_kriteria = '$_POST[id]'");
