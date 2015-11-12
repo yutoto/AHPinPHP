b2df8358b990a24599934dccdf9ddee4a2e027a5
Adi <undip.nugroho@gmail.com>
2015-11-13 01:17:58 +1030

    Update aksi_matrik.php

--- decision/modul_admin/mod_bobot/aksi_matrik.php
+++ decision/modul_admin/mod_bobot/aksi_matrik.php
@@ -6,11 +6,10 @@ $modul=$_GET[modul];
 $act=$_GET[act];
 $jlhkriteria = mysql_result(mysql_query("SELECT COUNT(*) as Num FROM kriteria"),0); // Jumlah Kriteria
 
-// Hitung Matrik Perbandingan
+// Create the Pairwise Comparison Judgement Matrix
 if ($modul=='bobot' AND $act=='matrikperbandingan'){
-  // Membuat Matrik Perbandingan
   // =========================================================
-  // Step 1 : Menyimpan Nilai Diagonal Matrik Perbandingan = 1
+  // Step 1 : Creating the diagonal value of matrix = 1
   for ($i=1; $i<=$jlhkriteria; $i++){
 	for ($j=$i; $j<$i+1; $j++){
 		$M[$i][$j] = 1;
@@ -18,7 +17,7 @@ if ($modul=='bobot' AND $act=='matrikperbandingan'){
 		mysql_query("INSERT INTO matrik_kriteria(id_kriteria,id_bandingan,nilai) VALUES('$i', '$j', '$diagonal')");
 	}
   }
-  // Step 2 : Matriks perbandingan kanan atas
+  // Step 2 : Creating the right - top part of the matrix
   for ($i=1; $i<=$jlhkriteria; $i++){
 	for ($j=$i+1; $j<=$jlhkriteria; $j++){
 		$u = $_POST['Matriks'.$i.$j];
@@ -33,7 +32,7 @@ if ($modul=='bobot' AND $act=='matrikperbandingan'){
 		mysql_query("INSERT INTO matrik_kriteria(id_kriteria,id_bandingan,nilai) VALUES('$i', '$j', '$kananatas')");
 	}
   }
-  // Step 3 : Mencari nilai matriks kiri bawah
+  // Step 3 : Counting the value of left - bottom of matrix
   for ($i=$jlhkriteria; $i>0; $i--){ // matriks kiri bawah
 	$M[$i][$i] = 1;
 	for ($j=$i-1; $j>0; $j--){
@@ -44,15 +43,15 @@ if ($modul=='bobot' AND $act=='matrikperbandingan'){
 	}
   }
   
-  // Sintesis
+  // Synthesis
   // =========================================================
-  // 1. Buat matrik transpose MT[i][j]
+  // 1. Create the Transpose Matrix MT[i][j]
 	for ($i=1;$i<=$jlhkriteria;$i++){
 		for ($j=1;$j<=$jlhkriteria;$j++){
 			$MT[$i][$j] = $M[$j][$i];
 		}
 	}
-	// 2. Jumlahkan setiap baris matrik transpose MT
+	// 2. Sum up each line of the Transpose Matrix MT
 	for ($i=1;$i<=$jlhkriteria;$i++){
 		$total=0;
 		for ($j=1;$j<=$jlhkriteria;$j++){
@@ -60,7 +59,7 @@ if ($modul=='bobot' AND $act=='matrikperbandingan'){
 		}
 		$sum[$i]=$total;
 	}
-	// 3. Buat Matrik Normalisasi MC[i][j]
+	// 3. Create Normalization Matrix MC[i][j]
 	for ($i=1;$i<=$jlhkriteria;$i++){
 		for ($j=1;$j<=$jlhkriteria;$j++){
 			$MC[$i][$j] = round (($MT[$i][$j] / $sum[$i]),3);
@@ -68,7 +67,7 @@ if ($modul=='bobot' AND $act=='matrikperbandingan'){
 			mysql_query("INSERT INTO matrik_normalisasi_kriteria(id_kriteria,id_bandingan,nilai) VALUES('$i', '$j', '$normalisasi')");
 		}
 	}
-	// 4. Jumlahkan setiap baris matrik MC[i][j]
+	// 4. Sum up each line in the Normalization Matrix MC[i][j]
 	for ($i=1;$i<=$jlhkriteria;$i++){
 		$totalc=0;
 		for ($j=1;$j<=$jlhkriteria;$j++){
@@ -76,7 +75,7 @@ if ($modul=='bobot' AND $act=='matrikperbandingan'){
 		}
 		$sumc[$i]=$totalc;
 	}
-	// Prioritas kriteria
+	// Counting the Priority of Criteria
 	for ($i=1;$i<=$jlhkriteria;$i++){
 		$ave[$i] = round (($sumc[$i] / $jlhkriteria),3);
 		$nilai = $ave[$i];
@@ -84,7 +83,7 @@ if ($modul=='bobot' AND $act=='matrikperbandingan'){
 		mysql_query("UPDATE bobot_kriteria SET bobot = '$nilai' WHERE id_kriteria = '$i'");
 	}
 	
-	// Menghitung Rasio Konsistensi (CR)
+	// Counting Consistency Ratio (CR)
   // =========================================================
   // 1. Matrik MB[i][j]
 	for ($i=1;$i<=$jlhkriteria;$i++){
@@ -92,7 +91,7 @@ if ($modul=='bobot' AND $act=='matrikperbandingan'){
 			$MB[$i][$j] = round (($MT[$i][$j] * $ave[$i]),3);
 		}
 	}
-	// 2. Jumlahkan tiap baris matrik MB[j][i]
+	// 2. Sum up each line of matrik MB[j][i]
 	for ($i=1;$i<=$jlhkriteria;$i++){
 		$totalb=0;
 		for ($j=1;$j<=$jlhkriteria;$j++){
@@ -100,16 +99,16 @@ if ($modul=='bobot' AND $act=='matrikperbandingan'){
 		}
 		$sumb[$i]=$totalb;
 	}
-	// 3. Bagi sumb dengan ave
+	// 3. Divide "sumb" with "ave"
 	for ($i=1;$i<=$jlhkriteria;$i++){
 		$tot[$i] = round (($sumb[$i] / $ave[$i]),3);
 	}
-	// 4. Jumlahkan semua nilai tot
+	// 4. Sum up all total value
 	$jumlah=0;
 	for ($i=1;$i<=$jlhkriteria;$i++){
 		$jumlah += $tot[$i];
 	}
-	// 5. Hitung lamda maks
+	// 5. Count the Lambda Max
 	$lamda = round (($jumlah/$jlhkriteria),3);
 	// 6. Hitung CI
 	$ci = round ((($lamda-$jlhkriteria)/($jlhkriteria-1)),3);
@@ -152,12 +151,12 @@ if ($modul=='bobot' AND $act=='matrikperbandingan'){
   header('location:../../indexs.php?modul='.$modul);
 }
 
-// Lanjut ke evaluasi karyawan
+// Straight to the Employee Evaluation
 elseif ($modul=='bobot' AND $act=='lanjutkan'){
   header('location:../../indexs.php?modul=evaluasi');
 }
 
-// hitung lagi
+// Redo the computation
 elseif ($modul=='bobot' AND $act=='hitunglagi'){
   mysql_query("DELETE FROM konsistensi");
   mysql_query("DELETE FROM matrik_kriteria");
