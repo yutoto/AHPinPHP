0ffd46bf8f2adbcd646fab10baede452c584d934
Adi <undip.nugroho@gmail.com>
2015-11-12 22:30:21 +1030

    Create bantuan.php

---
+++ decision/bantuan.php
@@ -0,0 +1,24 @@
+<?php
+
+echo "<h2>Guideline to the system</h2>";
+echo "Still under construction.";
+/*	
+echo "
+	1. Menu Admin > Data Karyawan , digunakan untuk menambah data-data pribadi dan akun karyawan
+	<br>
+	2. Menu Admin > Kriteria Penilaian , digunakan untuk menambah,update dan hapus kriteria penilaian
+	<br>
+	3. Menu Admin > Bobot Kriteria, digunakan untuk menghitung bobot penilaian masing-masing kriteria
+	<br>
+	4. Menu Admin > Skor Penilaian, digunakan untuk menghitung total nilai penilaian masing-masing karyawan
+	<br>
+	5. Menu Admin dan User > Laporan Evaluasi Kinerja, digunakan untuk menampilkan hasil perankingan karyawan
+	<br>
+	6. Menu Admin dan User > Bantuan, digunakan untuk melihat petunjuk penggunaan navigasi website
+	<br>
+	7. Menu Admin dan User > Tentang SPK, digunakan untuk melihat deskripsi SPK dan referensinya 
+	<br>
+	8. Menu User > Profil, digunakan oleh user untuk update data pribadi atau akun SPK 
+";
+*/
+?>
