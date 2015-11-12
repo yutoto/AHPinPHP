73f158baa1bac1c8d7938f5072198729238aee16
Adi <undip.nugroho@gmail.com>
2015-11-12 23:16:34 +1030

    Create aksi_karyawan.php

---
+++ decision/modul_admin/mod_karyawan/aksi_karyawan.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+include "../../config/koneksi.php";
+
+$modul=$_GET[modul];
+$act=$_GET[act];
+
+// Hapus Karyawan
+if ($modul=='dtakary' AND $act=='hapus'){
+  mysql_query("DELETE FROM karyawan WHERE nip='$_GET[id]'");
+  mysql_query("DELETE FROM pengguna WHERE nip='$_GET[id]'");
+  mysql_query("DELETE FROM evaluasi WHERE nip='$_GET[id]'");
+  mysql_query("DELETE FROM hasil_evaluasi WHERE nip='$_GET[id]'");
+  header('location:../../indexs.php?modul='.$modul);
+}
+
+// Input Karyawan
+elseif ($modul=='dtakary' AND $act=='input'){
+  $pass=md5($_POST[password]);
+  mysql_query("INSERT INTO karyawan(
+  								 nip,
+                                 nama_karyawan,
+								 jabatan,
+								 divisi,
+                                 alamat, 
+                                 telp) 
+	                       VALUES('$_POST[nip]',
+                                '$_POST[nama_karyawan]',
+								'$_POST[jabatan]',
+								'$_POST[divisi]',
+                                '$_POST[alamat]',
+                                '$_POST[telp]')");
+  mysql_query("INSERT INTO pengguna(
+                                 nip,
+  								 username,
+								 password
+								 ) 
+	                       VALUES('$_POST[nip]',
+                                '$_POST[username]',
+								'$pass'
+								)");
+  header('location:../../indexs.php?modul='.$modul);
+}
+
+// Update Karyawan
+elseif ($modul=='dtakary' AND $act=='update'){
+  if (empty($_POST[password])) {
+    mysql_query("UPDATE karyawan SET 
+                                  nama_karyawan   = '$_POST[nama_karyawan]',
+								  jabatan			 = '$_POST[jabatan]',
+								  divisi		 = '$_POST[divisi]',
+                                  alamat          = '$_POST[alamat]',
+                                  telp         = '$_POST[telp]'  
+                           WHERE  nip       = '$_POST[id]'");
+  }
+  // Apabila password diubah
+  else{
+    $pass=md5($_POST[password]);
+	mysql_query("UPDATE karyawan SET nip       = '$_POST[nip]',
+                                  nama_karyawan   = '$_POST[nama_karyawan]',
+								  jabatan			 = '$_POST[jabatan]',
+								  divisi		 = '$_POST[divisi]',
+                                  alamat          = '$_POST[alamat]',
+                                  telp         = '$_POST[telp]'  
+                           WHERE  nip       = '$_POST[id]'");
+    mysql_query("UPDATE pengguna SET password = '$pass' 
+                           WHERE  username       = '$_POST[id1]'");
+  }
+  header('location:../../indexs.php?modul='.$modul);
+}
+?>
+
