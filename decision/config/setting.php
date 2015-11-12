22a2417cea406c64dfb3c581da50f2a97d8b47d5
Adi <undip.nugroho@gmail.com>
2015-11-12 22:38:15 +1030

    Create setting.php

---
+++ decision/config/setting.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+KONFIGURASI SPK EVALUASI KINERJA KARYAWAN
+*/
+
+$title = "DSS for Employee Assessment";
+$header = "DSS for Employee Assessment of DG Treasury <br> Using AHP Method";
+$footer = "Flinders University CSEM 2015";
+
+?>
