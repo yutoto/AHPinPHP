929d034b648d28d0edbd2200d074106b8091539a
Adi <undip.nugroho@gmail.com>
2015-11-12 22:36:50 +1030

    Create tentang.php

---
+++ decision/tentang.php
@@ -0,0 +1,28 @@
+<?php
+
+echo "<h2>About the Decision Support System</h2>";
+echo "Still under construction";
+/*
+echo "
+	Ebook Referensi untuk mempelajari SPK :<br>
+	Subakti, Irfan. <a href='http://is.its-sby.edu/subjects/dss/Buku_Panduan_SPK.pdf'>Sistem Pendukung Keputusan.</a><br>
+	<br>
+	Buku Referensi untuk mempelajari SPK dan AHP :<br>
+	Kusrini. 2007. Konsep Dan Aplikasi Sistem Pendukung Keputusan. ANDI. Yogyakarta<br>
+	<br>
+	Website Referensi untuk mempelajari Multicriteria Decision Making and AHP :<br>
+	Teknomo, Kardi. <a href='http://people.revoledu.com/kardi/tutorial/AHP/AHP-Resources.htm'>Analytic Hierarchy Process</a><br>
+	<br>
+	Jurnal Referensi Analytical Hierarchy Process :<br>
+	Saaty. <a href='http://www.colorado.edu/geography/leyk/geog_5113/readings/saaty_2008.pdf'>Decision Making With Analytic Hierarchy Process. Int. J. Services Sciences, Vol. 1, No. 1, 2008</a><br>
+	<br>
+	Salah Satu Skripsi SPK Menggunakan AHP :<br>
+	Bambang Eka Putra. <a href='http://www.4shared.com/get/173659689/2e8aca2e/SISTEM_PENDUKUNG_PENILAIAN_KIN.html'>Sistem Pendukung Penilaian Karyawan</a>. Skripsi. Universitas Islam Indonesia. Yogyakarta.<br>
+	<br>
+	Contoh Sistem Pendukung Keputusan Menggunakan AHP Berbasis Web :<br>
+	<a href='http://kroseva.wordpress.com'>SPK Evaluasi Kinerja Karyawan PT XYZ Menggunakan Analytical Hierarchy Process</a><br>
+	<br>
+	Info lebih detail silahkan cari di google.
+";
+*/
+?>
