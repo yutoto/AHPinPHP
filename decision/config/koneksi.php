aea6d9488af1c734955d422b1e0aa6ce641555e4
Adi <undip.nugroho@gmail.com>
2015-11-13 00:31:14 +1030

    Update koneksi.php

--- decision/config/koneksi.php
+++ decision/config/koneksi.php
@@ -4,7 +4,7 @@ $username = "root";
 $password = "";
 $database = "decision";
 
-// Koneksi dan memilih database di server
+// creating connection and choosing database available in server
 $con = mysql_connect($server,$username,$password) or die("Connection Failed");
 mysql_select_db($database, $con) or die("Can't Access the Database");
 ?>
