52a1f505cbac77763c19b55362f5b02edc4e90b0
Adi <undip.nugroho@gmail.com>
2015-11-12 23:15:22 +1030

    Create aksi_evaluasi.php

---
+++ decision/modul_admin/mod_evaluasi/aksi_evaluasi.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+include "../../config/koneksi.php";
+
+$modul=$_GET[modul];
+$acts=$_GET[acts];
+$jlhkriteria=$_POST[jlhkriteria];
+$id=$_POST[nip];
+// Input nilai
+if ($modul=='evaluasi' AND $acts=='input'){
+  for($i=1;$i<=$jlhkriteria;$i++){
+  	$nilai = $_POST['nilai'.$i];
+	$id_kriteria = $_POST['id_kriteria'.$i];
+  	mysql_query("INSERT INTO evaluasi(nip, id_kriteria, nilai) VALUES('$_POST[nip]', '$id_kriteria', '$nilai')");
+  }
+  
+  //Total Nilai
+  $total=0;
+  for($i=1;$i<=$jlhkriteria;$i++){
+  	$nilai = $_POST['nilai'.$i];
+	$id_kriteria = $_POST['id_kriteria'.$i];
+	$tampil=mysql_query("SELECT bobot FROM bobot_kriteria WHERE id_kriteria='$id_kriteria' ORDER BY id_kriteria ASC");
+	$r=mysql_fetch_array($tampil);
+	$skor_total=$nilai*$r[bobot];
+	$total += $skor_total;
+  }
+  
+  mysql_query("INSERT INTO previous_calculation(nip, total_nilai, datemade) VALUES('$_POST[nip]', '$total', now())");
+    mysql_query("INSERT INTO hasil_evaluasi(nip, total_nilai, datemade) VALUES('$_POST[nip]', '$total', now())");
+    header('location:../../indexs.php?modul='.$modul);
+  
+}
+
+// Update nilai
+elseif ($modul=='evaluasi' AND $acts=='update'){
+  for($i=1;$i<=$jlhkriteria;$i++){
+  	$nilai = $_POST['nilai'.$i];
+	$id_kriteria = $_POST['id_kriteria'.$i];
+	mysql_query("UPDATE evaluasi SET nip = '$_POST[nip]', id_kriteria = '$id_kriteria', nilai = '$nilai' WHERE nip = '$_POST[nip]' AND id_kriteria = '$id_kriteria'");  
+	
+  }
+  
+  //Total Nilai
+  $total=0;
+  for($i=1;$i<=$jlhkriteria;$i++){
+  	$nilai = $_POST['nilai'.$i];
+	$id_kriteria = $_POST['id_kriteria'.$i];
+	$tampil=mysql_query("SELECT bobot FROM bobot_kriteria WHERE id_kriteria='$id_kriteria'");
+	$r=mysql_fetch_array($tampil);
+	$skor_total=$nilai*$r[bobot];
+	$total += $skor_total;
+  }  
+  
+  $eval_code = $_POST['eval_code'];
+  mysql_query("INSERT INTO hasil_evaluasi(nip, total_nilai, eval_code, datemade) VALUES('$_POST[nip]', '$total', '$eval_code', now())");  
+  mysql_query("INSERT INTO previous_calculation(nip, total_nilai, eval_code, datemade) VALUES('$_POST[nip]', '$total', '$eval_code', now())");  
+  header('location:../../indexs.php?modul='.$modul.'');
+  
+}
+?>
+
